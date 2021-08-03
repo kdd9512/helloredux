79c94ebbfeeed93fac5cca9a20f66fc9a55ad3c5
kdd9512 <hyunwoo8805@gmail.com>
2021-08-03 17:26:08 +0900

    20210803 commit02
    Vanilla Redux
    Learning Vanilla-Redux & React-Redux
    
    TODOS using REDUX

--- src/index.js
+++ src/index.js
@@ -1,40 +1,82 @@
 import {createStore} from "redux";
 
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const num = document.querySelector("span");
 
-num.innerText = 0;
+// 01. intro
+//
+// const plus = document.getElementById("plus");
+// const minus = document.getElementById("minus");
+// const num = document.querySelector("span");
+//
+// num.innerText = 0;
+//
+// const PLUS = "plus";
+// const MINUS = "minus";
+//
+// const countModifier = (cnt = 0, action) => {
+//     switch (action.type) {
+//         case PLUS:
+//             return cnt + 1
+//         case MINUS:
+//             return cnt - 1
+//         default:
+//             return ;
+//     }
+//
+// };
+// const countStore = createStore(countModifier);
+//
+// const onChange = () => {
+//     num.innerText = countStore.getState();
+// };
+//
+// countStore.subscribe(onChange);
+//
+// const handlePlus = () => {
+//     countStore.dispatch({type: PLUS})
+// };
+//
+// const handleMinus = () => {
+//     countStore.dispatch({type: MINUS})
+// };
+//
+// plus.addEventListener("click", handlePlus);
+// minus.addEventListener("click", handleMinus);
 
-const PLUS = "plus";
-const MINUS = "minus";
 
-const countModifier = (cnt = 0, action) => {
+// 02. TODOS using REDUX
+
+const ADD = "add";
+const DEL = "del";
+
+const reducer = (state = [], action) => {
     switch (action.type) {
-        case PLUS:
-            return cnt + 1
-        case MINUS:
-            return cnt - 1
+        case ADD:
+            return []
+        case DEL:
+            return []
         default:
-            return ;
+            return state;
     }
-
-};
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-    num.innerText = countStore.getState();
 };
 
-countStore.subscribe(onChange);
+const store = createStore(reducer);
 
-const handlePlus = () => {
-    countStore.dispatch({type: PLUS})
-};
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const handleMinus = () => {
-    countStore.dispatch({type: MINUS})
-};
 
-plus.addEventListener("click", handlePlus);
-minus.addEventListener("click", handleMinus);
+const createTodo = toDo => {
+    const li = document.createElement("li");
+    li.innerText = toDo;
+    ul.appendChild(li);
+}
+
+const onSubmit = e => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    store.dispatch({type:ADD, text:toDo});
+}
+
+form.addEventListener("submit", onSubmit);
