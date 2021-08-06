02422079c5c95231b5ecc1c8091a4d1ea75f860e
kdd9512 <hyunwoo8805@gmail.com>
2021-08-06 22:44:20 +0900

    20210803 commit03
    Vanilla Redux
    Learning Vanilla-Redux & React-Redux
    
    TODOS using REACT-REDUX.
    shorten code using REDUX-TOOLKIT

--- src/store.js
+++ src/store.js
@@ -1,41 +1,34 @@
 import {createStore} from "redux";
-import {createAction} from "@reduxjs/toolkit";
-
-const ADD = "ADD";
-const DEL = "DEL";
-
-// const addTodo = text => {
-//     return {
-//         type:ADD,
-//         text
-//     }
-// }
-//
-// const delTodo = id => {
-//     return {
-//         type:DEL,
-//         id
-//     }
-// }
+import {createAction, createReducer} from "@reduxjs/toolkit";
 
 // redux toolkit 이용하여 addTodo,delTodo 단축.
 const addTodo = createAction("ADD");
 const delTodo = createAction("DEL");
 
 
-const reducer = (state = [],action) => {
-    switch (action.type) {
-        case addTodo.type:
-            console.log(action);
-            return [{text:action.payload, id: Date.now()}, ...state];
+// const reducer = (state = [],action) => {
+//     switch (action.type) {
+//         case addTodo.type:
+//             console.log(action);
+//             return [{text:action.payload, id: Date.now()}, ...state];
+//
+//         case delTodo.type:
+//             console.log(action);
+//             return state.filter(toDo => toDo.id !== action.payload);
+//         default:
+//             return state;
+//     }
+// }
 
-        case delTodo.type:
-            console.log(action);
-            return state.filter(toDo => toDo.id !== action.payload);
-        default:
-            return state;
-    }
-}
+// redux toolkit 이용하여 reducer 단축.
+const reducer = createReducer([], {
+    [addTodo]: (state,action) => {
+        // toolkit을 사용하는 경우, .push 를 사용할 수 있게된다.
+        state.push({text:action.payload, id: Date.now()});
+    },
+    [delTodo]: (state,action) =>
+        state.filter(toDo => toDo.id !== action.payload)
+})
 
 const store = createStore(reducer);
 
