05c07313f770eecbb3d14244229fc48c222816ca
kdd9512 <hyunwoo8805@gmail.com>
2021-08-06 22:32:37 +0900

    20210803 commit03
    Vanilla Redux
    Learning Vanilla-Redux & React-Redux
    
    TODOS using REACT-REDUX.
    shorten code using REDUX-TOOLKIT

--- src/store.js
+++ src/store.js
@@ -1,28 +1,37 @@
 import {createStore} from "redux";
+import {createAction} from "@reduxjs/toolkit";
 
 const ADD = "ADD";
 const DEL = "DEL";
 
-const addTodo = text => {
-    return {
-        type:ADD,
-        text
-    }
-}
+// const addTodo = text => {
+//     return {
+//         type:ADD,
+//         text
+//     }
+// }
+//
+// const delTodo = id => {
+//     return {
+//         type:DEL,
+//         id
+//     }
+// }
 
-const delTodo = id => {
-    return {
-        type:DEL,
-        id
-    }
-}
+// redux toolkit 이용하여 addTodo,delTodo 단축.
+const addTodo = createAction("ADD");
+const delTodo = createAction("DEL");
+
 
 const reducer = (state = [],action) => {
     switch (action.type) {
-        case ADD:
-            return [{text:action.text, id: Date.now()}, ...state];
-        case DEL:
-            return state.filter(toDo => toDo.id !== action.id);
+        case addTodo.type:
+            console.log(action);
+            return [{text:action.payload, id: Date.now()}, ...state];
+
+        case delTodo.type:
+            console.log(action);
+            return state.filter(toDo => toDo.id !== action.payload);
         default:
             return state;
     }
