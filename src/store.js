557a707af3f541cc5fe6b9fb7bb45878264149df
kdd9512 <hyunwoo8805@gmail.com>
2021-08-08 21:27:45 +0900

    20210803 commit03
    Vanilla Redux
    Learning Vanilla-Redux & React-Redux
    
    TODOS using REACT-REDUX.
    shorten code using REDUX-TOOLKIT

--- src/store.js
+++ src/store.js
@@ -1,19 +1,6 @@
-import {configureStore, createAction, createReducer, createSlice} from "@reduxjs/toolkit";
+import {configureStore, createSlice} from "@reduxjs/toolkit";
 
-// // redux toolkit 이용하여 addTodo,delTodo 단축.
-// const addTodo = createAction("ADD");
-// const delTodo = createAction("DEL");
-//
-// // redux toolkit 이용하여 reducer 단축.
-// const reducer = createReducer([], {
-//     [addTodo]: (state,action) => {
-//         // toolkit을 사용하는 경우, .push 를 사용할 수 있게된다.
-//         state.push({text:action.payload, id: Date.now()});
-//     },
-//     [delTodo]: (state,action) =>
-//         state.filter(toDo => toDo.id !== action.payload)
-// })
-
+// createSlice 이용하여 reducer, action 을 압축.
 const toDo = createSlice({
     name:"toDosReducer",
     initialState:[],
@@ -27,10 +14,8 @@ const toDo = createSlice({
     }
 });
 
-
 // Redux dev tools 를 사용하기 위해 configureStore 를 사용.
 const store = configureStore({reducer:toDo.reducer});
 
 export const {add,del} = toDo.actions;
-
 export default store;
